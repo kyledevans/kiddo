bc4cdda916f7adb03aafd0efba11c7711035ae30
Kyle Evans <kyledevans@gmail.com>
2022-06-12 13:49:47 -0500

    Prometheus metrics.
    Expose basic prometheus metrics.  Remove the App.Metrics package because Prometheus is pretty much the industry standard for monitoring.

--- Kiddo.Web/Program.cs
+++ Kiddo.Web/Program.cs
@@ -1,29 +1,12 @@
-using Microsoft.Identity.Web;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Serilog;
 using Prometheus;
-using App.Metrics;
-using App.Metrics.AspNetCore;
-using App.Metrics.AspNetCore.Tracking;
-using App.Metrics.Reporting.InfluxDB;
 using Kiddo.DAL;
 using Kiddo.Clients.BackgroundServiceClient;
 using Kiddo.Web.Security;
 using Kiddo.Web.Mappers;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Graph.ExternalConnectors;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Net.Http.Headers;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.OpenApi.Models;
 using Kiddo.Web.Configuration;
-
-MetricsFrameworkType metricsFramework = MetricsFrameworkType.None;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -94,35 +77,7 @@ try
         .AddDbContextCheck<KiddoDbContextExtended>("kiddodb", Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Degraded, new string[] { "readiness" });
 
     // Add metrics
-    if (metricsFramework == MetricsFrameworkType.PrometheusNet)
-    {
-        // prometheus-net
-        healthChecksBuilder.ForwardToPrometheus();
-    }
-    else if (metricsFramework == MetricsFrameworkType.AppMetrics)
-    {
-        // App.Metrics
-        // TODO: This is using a reporter designed against InfluxDB v1.  There is a new version that is about
-        // to be released (as of 2022-01-08) that supports the latest version of InfluxDb.  Once that becomes
-        // available this code should be swapped out.
-        // 
-        // Also I'm not sure if App.Metrics is actually pulling *any* settings from appsettings.json.  It's
-        // a bit unclear from the documentation and I wasn't able to get InfluxDb specific configuration to
-        // pull from there.  Probably will require looking into the App.Metrics code a bit to figure out how
-        // this is supposed to be accomplished.
-        builder.Host.ConfigureMetricsWithDefaults(config => {
-            config.Report.ToInfluxDb(influx => {
-                influx.InfluxDb.Database = "appmetrics";
-                influx.InfluxDb.UserName = "kiddo";
-                influx.InfluxDb.Password = "secure_password";
-                influx.InfluxDb.RetentionPolicy = "kiddo";
-                influx.InfluxDb.BaseUri = new("http://localhost:8086");
-                influx.InfluxDb.CreateDataBaseIfNotExists = true;
-            });
-        }).UseMetrics();
-        builder.Services.AddAppMetricsHealthPublishing();
-        builder.Services.AddAppMetricsCollectors();
-    }
+    healthChecksBuilder.ForwardToPrometheus();
 
     // Add implementations for backend abstractions
     builder.Services.AddScoped<ICurrentUserProvider, CurrentUserProvider>();
@@ -157,36 +112,20 @@ try
 
     app.UseSerilogRequestLogging();
 
-    if (!app.Environment.IsDevelopment())
-    {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //app.UseHsts();
-    }
-    else if (app.Environment.IsDevelopment())
+    if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
 
-    if (app.Environment.IsDevelopment())
-    {
-        //app.UseHttpsRedirection();
-    }
-
     app.UseStaticFiles();
     app.UseRouting();
-
-    if (metricsFramework == MetricsFrameworkType.PrometheusNet)
-    {
-        app.UseHttpMetrics();
-    }
-
+    app.UseHttpMetrics();
     app.UseAuthenticationMethodEnablementMiddleware();
     app.UseAuthentication();
     app.UseAuthorization();
     app.UseEmailRequiredMiddleware();
     app.MapControllers();
-
     app.UseEndpoints(endpoints =>
     {
         endpoints.MapHealthChecks("/health");
@@ -200,10 +139,7 @@ try
             Predicate = healthCheck => healthCheck.Tags.Contains("readiness")
         });
 
-        if (metricsFramework == MetricsFrameworkType.PrometheusNet)
-        {
-            endpoints.MapMetrics();
-        }
+        endpoints.MapMetrics();
     });
 
     app.MapFallbackToFile("index.html");
@@ -220,22 +156,4 @@ finally
 {
     Log.Information("Kiddo.Web shut down complete.");
     Log.CloseAndFlush();
-}
-
-enum MetricsFrameworkType
-{
-    /// <summary>
-    /// No application metrics will be collected or reported.
-    /// </summary>
-    None = 0,
-
-    /// <summary>
-    /// prometheus-net framework: https://github.com/prometheus-net/prometheus-net
-    /// </summary>
-    PrometheusNet = 1,
-
-    /// <summary>
-    /// App.Metrics framework: https://github.com/AppMetrics/AppMetrics
-    /// </summary>
-    AppMetrics = 2
 }
