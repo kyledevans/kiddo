4ffd3f425a740bf3e0558276ac9dbae5afc50dae
Kyle Evans <kevans@compumedinc.com>
2025-02-09 20:16:14 -0800

    WIP.

--- Frontend/src/common/error-callout.tsx
+++ Frontend/src/common/error-callout.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, FunctionComponent, MutableRefObject } from "react";
+import { useCallback, useEffect, useRef, FunctionComponent, MutableRefObject, ReactNode } from "react";
 import { IIconProps, Callout, Icon, Target } from "@fluentui/react";
 import { mergeStyleSets } from "@fluentui/react/lib/Styling";
 import { useBoolean } from "@fluentui/react-hooks";
@@ -8,22 +8,22 @@ const errorCalloutStyles = mergeStyleSets({
     cursor: "default",
     selectors: {
       ".ms-Callout-beak": {
-        backgroundColor: "rgb(253, 161, 163)"
+        backgroundColor: "rgb(253, 161, 163)",
       },
       ".ms-Callout-beakCurtain": {
-        backgroundColor: "rgb(253, 161, 163)"
+        backgroundColor: "rgb(253, 161, 163)",
       },
       ".ms-Callout-main": {
         display: "grid",
         gridTemplateColumns: "max-content min-content",
         backgroundColor: "rgb(253, 161, 163)",
-        padding: "8px 16px"
-      }
-    }
+        padding: "8px 16px",
+      },
+    },
   },
   errorIcon: {
-    padding: "0 0 0 8px"
-  }
+    padding: "0 0 0 8px",
+  },
 });
 
 const errorCalloutIcon: IIconProps = { iconName: "ErrorBadge" };
@@ -33,33 +33,40 @@ export interface ErrorCalloutControl {
   open: (duration: number) => void;
 }
 
-export const ErrorCallout: FunctionComponent<{ target: Target, control: MutableRefObject<ErrorCalloutControl | null> }> = ({ target, control, children }) => {
+export const ErrorCallout: FunctionComponent<{ target: Target; control: MutableRefObject<ErrorCalloutControl | null>; children?: ReactNode }> = ({
+  target,
+  control,
+  children,
+}) => {
   const [isVisible, { setTrue: setIsVisible, setFalse: setIsHidden }] = useBoolean(false);
   const cancelTimeoutRef = useRef<null | (() => void)>(null);
 
-  const open = useCallback((duration: number) => {
-    // Clear any pending timeouts.
-    if (cancelTimeoutRef.current != null) {
-      cancelTimeoutRef.current();
-    }
+  const open = useCallback(
+    (duration: number) => {
+      // Clear any pending timeouts.
+      if (cancelTimeoutRef.current != null) {
+        cancelTimeoutRef.current();
+      }
 
-    setIsVisible();
+      setIsVisible();
 
-    const timeoutHandle = setTimeout(() => {
-      setIsHidden();
-      cancelTimeoutRef.current = null;
-    }, duration);
+      const timeoutHandle = setTimeout(() => {
+        setIsHidden();
+        cancelTimeoutRef.current = null;
+      }, duration);
 
-    cancelTimeoutRef.current = () => {
-      clearTimeout(timeoutHandle);
-      cancelTimeoutRef.current = null;
-    };
-  }, [setIsVisible, setIsHidden, cancelTimeoutRef]);
+      cancelTimeoutRef.current = () => {
+        clearTimeout(timeoutHandle);
+        cancelTimeoutRef.current = null;
+      };
+    },
+    [setIsVisible, setIsHidden, cancelTimeoutRef],
+  );
 
   useEffect(() => {
     control.current = {
       dismiss: setIsHidden,
-      open: open
+      open: open,
     };
 
     return () => {
@@ -72,8 +79,11 @@ export const ErrorCallout: FunctionComponent<{ target: Target, control: MutableR
   return (
     <>
       {isVisible && (
-        <Callout dismissOnTargetClick={true} target={target} onDismiss={setIsHidden} onClick={setIsHidden} className={errorCalloutStyles.callout}>{children}<Icon className={errorCalloutStyles.errorIcon} iconName={errorCalloutIcon.iconName} /></Callout>
+        <Callout dismissOnTargetClick={true} target={target} onDismiss={setIsHidden} onClick={setIsHidden} className={errorCalloutStyles.callout}>
+          {children}
+          <Icon className={errorCalloutStyles.errorIcon} iconName={errorCalloutIcon.iconName} />
+        </Callout>
       )}
     </>
   );
-}
+};
